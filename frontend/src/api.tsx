2fb57031a316deee63fa87d90b225d01095d237c
shin-ai <muhammad.rifanny15@gmail.com>
2025-05-14 17:31:36 +0700

    test fetch Financial Modeling Prep API

---
+++ frontend/src/api.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import type { CompanySearch } from "./company";
+
+interface SearchResponse {
+  data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected: ", error);
+      return "An expected error has occured";
+    }
+  }
+};
