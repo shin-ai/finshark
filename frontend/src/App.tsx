e3927879bc47f541966ff6030559dc734cc51766
shin-ai <muhammad.rifanny15@gmail.com>
2025-05-14 16:01:41 +0700

    add Search.tsx and Search.css and implement state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
