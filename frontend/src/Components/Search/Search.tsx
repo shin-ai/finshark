c2f4724563978145fbeb37c840c95b552cb8ee4c
shin-ai <muhammad.rifanny15@gmail.com>
2025-05-14 16:19:30 +0700

    implement event handlers to search

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,22 @@
-import React, { useState, type JSX } from "react";
+import React, { useState, type JSX, type SyntheticEvent } from "react";
 
 type Props = {};
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
   const [search, setSearch] = useState<string>("");
 
-  const onClick = (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const onClick = (e: SyntheticEvent) => {
     console.log(e);
   };
   return (
     <div>
-      <input value={search} onChange={(e) => onClick(e)}></input>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
     </div>
   );
 };
