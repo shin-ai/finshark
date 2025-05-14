95489e776417d2e9e749c78afddc71d1acd99af5
shin-ai <muhammad.rifanny15@gmail.com>
2025-05-14 13:13:25 +0700

    create card.tsx

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+const Card = (props: Props) => {
+  return (
+    <div className="card">
+      <img
+        src="https://images.unsplash.com/photo-1612428978260-2b9c7df20150?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+        alt="Image"
+      />
+    </div>
+  );
+};
+
+export default Card;
