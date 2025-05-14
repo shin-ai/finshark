eb8a7d928ad9d399d29cc148434862ca739d3d42
shin-ai <muhammad.rifanny15@gmail.com>
2025-05-14 14:34:41 +0700

    implement props on Card.tsx and add CardList.tsx and CardList.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import Card from "./Components/Card/Card";
+import CardList from "./Components/CardList/CardList";
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <CardList />
     </div>
   );
 }
